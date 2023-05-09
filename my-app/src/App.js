beccb0d7a9df289f2d9ebea6ce057e815948e934
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2023-05-09 22:12:49 +0900

    setting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react" ;
 
 function App() {
+  // setToDo는 toDo를 수정하는 함수
+  // toDo는 input과 연결
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "")
+      return;
+    setToDo("");
+  };
+
+  console.log(toDo);
+  // form이 submit event를 가지고 있기 때문에 button을 누르면 submit이 된다.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}> 
+        <input 
+          onChange={onChange} 
+          value={toDo} 
+          type="text" 
+          placeholder="Write your to do6.." 
+        />
+        <button>Add to do</button>
+      </form>
     </div>
   );
 }
